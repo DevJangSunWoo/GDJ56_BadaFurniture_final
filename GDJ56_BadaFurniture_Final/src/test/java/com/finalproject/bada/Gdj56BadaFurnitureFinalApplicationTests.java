17108c07e3208371b35c573ef6f1442ddb696722
bxxxdx <qudeh911@gmail.com>
2023-01-19 09:57:51 +0900

    firstcommit

---
+++ GDJ56_BadaFurniture_Final/src/test/java/com/finalproject/bada/Gdj56BadaFurnitureFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finalproject.bada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gdj56BadaFurnitureFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
