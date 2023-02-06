a46630cc45f9d457b0b767b3a0ce38abbc0641dd
bxxxdx <qudeh911@gmail.com>
2023-02-06 17:51:03 +0900

    2023-02-06 [BD] AlertFactory undeposited 경우 작성중,.

--- GDJ56_BadaFurniture_Final/src/main/java/com/finalproject/bada/common/AlertFactory.java
+++ GDJ56_BadaFurniture_Final/src/main/java/com/finalproject/bada/common/AlertFactory.java
@@ -1,10 +1,15 @@
 package com.finalproject.bada.common;
 
+
 import java.text.DecimalFormat;
 
 import com.finalproject.bada.order.model.vo.OrderDetail;
+import com.finalproject.bada.order.model.vo.OrderSheet;
 import com.finalproject.bada.resell.model.vo.Resell;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class AlertFactory {
 	
 	public static String getAlertMsg(String path, String state, Object obj) {
@@ -20,16 +25,21 @@ public class AlertFactory {
 		case "progressState":
 			Resell updateResell = (Resell)obj;
 			msg = "<a href=\""+path+"/resell/read.do?resellNo="+updateResell.getResellNo()+"\">"+updateResell.getItem()+" 판매신청("+formatter.format(updateResell.getHopePrice()) +")"+"</a>"
-					+"의 상태가 <b>" + updateResell.getProgressState() + (updateResell.getProgressState().equals("수정요청")?"으":"") +"</b>로 변경되었습니다.";
+					+"의 판매상태가 <b>" + updateResell.getProgressState() + (updateResell.getProgressState().equals("수정요청")?"으":"") +"</b>로 변경되었습니다.";
 			break;
 		case "refundState" :
 			OrderDetail orderDetail = (OrderDetail)obj;
 			if(orderDetail.getRefundState() != null) {
-				String refundState = orderDetail.getRefundState().indexOf("반품")>0?"반품":"취소";
+				String refundState = orderDetail.getRefundState().contains("반품")?"반품":"취소";
+				log.debug(refundState);
 				msg = "<a href=\""+path+"/mypage/refund.do?orderDetailNo="+orderDetail.getOrderDetailNo()+"\">["+refundState+"]"+orderDetail.getProduct().getTitle()+"("+formatter.format(orderDetail.getProduct().getPrice()) +")"+"</a>"
-					+"의 처리상태가 <b>" + orderDetail.getRefundState() + "</b>로 변경되었습니다.";
+					+"의 "+refundState+"상태가 <b>" + orderDetail.getRefundState() + "</b>로 변경되었습니다.";
 			}
 			break;
+		case "undeposited" :
+			OrderSheet undepositedOrderSheet = (OrderSheet)obj;
+			
+			
 		default: 
 			break;
 		}
